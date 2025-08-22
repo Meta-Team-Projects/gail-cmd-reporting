9e1367aa7ca5ac4e1feac818f86129cfa6451e45
Shashank23-10 <shashankpandey2001@gmail.com>
2025-08-22 21:55:26 +0530

    apiIntegration1.1.9 Temp Selection Width Adjustment

--- src/components/TemplateSelection.jsx
+++ src/components/TemplateSelection.jsx
@@ -315,7 +315,8 @@ const TemplateSelection = ({
             <Box sx={{
                 //border: '1px solid red',
                 maxWidth: leftSidebarOpen ? '41.67vw' : '46.88vw',
-                transition: 'max-width 0.3s ease',
+                flex: `0 0 ${leftSidebarOpen ? '41.67vw' : '46.88vw'}`,
+                transition: 'width 0.3s ease',
                 display: 'flex',
                 flexDirection: 'column', 
                 gap: '0.417vw'
