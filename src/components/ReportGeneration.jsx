51825446bfeee0caf079e414dda68f763a77dbe3
Rish192 <rishabpraveen1902@gmail.com>
2025-09-03 12:29:57 +0530

    delete removed in report-gen

--- src/components/ReportGeneration.jsx
+++ src/components/ReportGeneration.jsx
@@ -988,11 +988,11 @@ const disabledYellowSx = {
                                                                 <VisibilityOffIcon sx={{ fontSize: '0.8333vw', color: '#081A33'}} />
                                                             )}
                                                         </IconButton> */}
-                                                        <IconButton size="small" onClick={(e) => { e.stopPropagation(); }}>
+                                                        {/* <IconButton size="small" onClick={(e) => { e.stopPropagation(); }}>
                                                             <Tooltip title='Delete' placement='bottom' arrow>
                                                                 <Delete sx={{ fontSize: '0.8333vw', color: '#f08a8a' }} />
                                                             </Tooltip>
-                                                        </IconButton>
+                                                        </IconButton> */}
                                                     </Box>
                                                 </ListItem>
                                             );
