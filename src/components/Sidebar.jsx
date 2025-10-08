2da11b94a5afd7ef9a764b73188a5ae746f5b4c3
Shashank23-10 <shashankpandey2001@gmail.com>
2025-10-08 07:57:26 +0530

    color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -115,7 +115,7 @@ const Sidebar = ({
                         minWidth: 0,
                         mr: open ? 2 : 'auto',
                         justifyContent: 'center',
-                        color: 'inherit',
+                        color: '#fff',
                     }}
                 >
                     {item.type === 'TOGGLE_NOTEPAD' ? (
@@ -148,7 +148,7 @@ const Sidebar = ({
                                     ? '#fff'
                                     : activeMenu === item.type
                                         ? '#fff'
-                                        : '#678092',
+                                        : '#fff',
                                 //Lighter theme
                                 /*color: item.type === 'TOGGLE_NOTEPAD'
                                     ? '#303030'
